c8c9acf728a0482278312028fda23f1a78348311
ErikRBjorklund <erbjorklund@icloud.com>
2023-04-17 18:17:52 -0400

    Final
    Final

--- website/src/Home.js
+++ website/src/Home.js
@@ -18,7 +18,10 @@ function Home() {
 
 
     useEffect(() => {
-        axios.get("http://localhost:3001/comments")
+        axios.get("http://192.241.145.155:3001/comments", {
+                headers: {
+                  'Access-Control-Allow-Origin': '*',
+                }})
             .then(function (response) {
                 console.log(response.data)
                 setComments(getAllComments(response.data));
@@ -30,7 +33,10 @@ function Home() {
 
     useEffect(() => {
         if (userCommented) {
-            axios.get("http://localhost:3001/comments")
+            axios.get("http://192.241.145.155:3001/comments", {
+                headers: {
+                  'Access-Control-Allow-Origin': '*',
+                }})
                 .then(function (response) {
                     console.log(response.data)
                     setComments(getAllComments(response.data));
@@ -62,7 +68,7 @@ function Home() {
         console.log(input);
         console.log(`ID: ${localStorage.getItem("id")}`)
         if (input.length > 0) {
-            axios.post('http://localhost:3001/comment', null, { params: { userid: localStorage.getItem("id"), commenttext: input } })
+            axios.post('http://192.241.145.155:3001/comment', null, { params: { userid: localStorage.getItem("id"), commenttext: input } })
                 .then(function (response) {
                     console.log("Worked")
                     console.log(response)
