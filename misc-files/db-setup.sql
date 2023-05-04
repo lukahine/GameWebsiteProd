6f194e219e0299b525e17aa22c5bcf4608281fd3
ErikRBjorklund <erbjorklund@icloud.com>
2023-05-03 22:10:30 -0400

    Update db-setup.sql

--- misc-files/db-setup.sql
+++ misc-files/db-setup.sql
@@ -43,14 +43,14 @@ INSERT INTO permissions (permissionName, permissionAdd, permissionEdit) VALUES (
 INSERT INTO games (GameID, GameName, GameDescription, url) VALUES (
     NULL,
   'Lucky Break',
-  'Dodge or destroy Lady Lucks falling dice. Getting hit by one will lower your health by the number on it, but jumping on top of one will heal you by that same amount, and net you some extra points based on the risk! But be careful, Lady Luck randomly changes your speed, jump height, starting health, and the difficulty of the level every round! Made by Cooper Ott.'
+  'Dodge or destroy Lady Lucks falling dice. Getting hit by one will lower your health by the number on it, but jumping on top of one will heal you by that same amount, and net you some extra points based on the risk! But be careful, Lady Luck randomly changes your speed, jump height, starting health, and the difficulty of the level every round! Made by Cooper Ott.',
   'https://v6p9d9t4.ssl.hwcdn.net/html/6227312/index.html'
 );
 
 INSERT INTO games (GameID, GameName, GameDescription, url) VALUES (
     NULL,
   'Sort the Court!',
-  'Give your decree in simple yes or no answers, and help the kingdom grow! Made by Graeme Borland'
+  'Give your decree in simple yes or no answers, and help the kingdom grow! Made by Graeme Borland',
   'https://v6p9d9t4.ssl.hwcdn.net/html/347310/index.html?v=1542780889'
 );
 
