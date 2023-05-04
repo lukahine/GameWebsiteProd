785dc19fa5ddb6ede7c8f44bd5abe3da698d3ffd
ErikRBjorklund <erbjorklund@icloud.com>
2023-05-04 00:17:05 -0400

    Cors fix hopefully

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(cors());
+app.use(cors({origin: '*'}));
 
 // var connection = mysql.createPool({
 //     host: "localhost",
