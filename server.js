2a7fdcf30af000cabbb93ea3a52074e6215cb6ff
ErikRBjorklund <erbjorklund@icloud.com>
2023-05-03 22:57:19 -0400

    Server Connected to SQL Server

--- server.js
+++ server.js
@@ -16,13 +16,22 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
+// var connection = mysql.createPool({
+//     host: "localhost",
+//     port: "3306",
+//     user: "root",
+//     password: "",
+//     database: "hosting_db",
+//     connectionLimit: 100,
+// });
+
 var connection = mysql.createPool({
-    host: "localhost",
-    port: "3306",
-    user: "root",
-    password: "",
+    host: "cooplukerb-do-user-13916037-0.b.db.ondigitalocean.com",
+    port: "25060",
+    user: "erikadmin",
+    password: "AVNS_yjEe4pMgD-L8jQn5Zqg",
     database: "hosting_db",
-    connectionLimit: 100,
+    connectionLimit: 100
 });
 
 app.get('/', (req, res) => {
